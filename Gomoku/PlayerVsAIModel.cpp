a2588827cd7ec5febcb31e09aa61fe5fb49ecb8f
guillaume plantie <guillaume.plantie@epitech.eu>
2016-01-15 16:42:52 +0100

    pions

--- Gomoku/PlayerVsAIModel.cpp
+++ Gomoku/PlayerVsAIModel.cpp
@@ -21,5 +21,16 @@ PlayerVsAIModel::PlayerVsAIModel(sf::RenderWindow *window, EventManager **eventM
 
 void PlayerVsAIModel::Clicked()
 {
+	sf::Texture texture;
 
+	texture.loadFromFile("../Images/Goban.bmp");
+	sf::Sprite sprite(texture);
+	sprite.setPosition(sf::Vector2f(240, 0));
+	_window->draw(sprite);
+
+	texture.loadFromFile("../Images/Blanc.bmp");
+	sf::Sprite sprite2(texture);
+	sprite2.setPosition(sf::Vector2f(0, 0));
+	_window->draw(sprite2);
+	_window->display();
 }

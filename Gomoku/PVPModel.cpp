d593381c489a5105f1d86b2d7289ebe8c88091a9
Thibault <woivrethibault@gmail.com>
2016-01-15 18:51:17 +0100

    Displaying pawns

--- Gomoku/PVPModel.cpp
+++ Gomoku/PVPModel.cpp
@@ -5,12 +5,19 @@ PVPModel::PVPModel(sf::RenderWindow *window, EventManager **eventManagerAddr)
 	_window = window;
 	_eventManagerAddr = eventManagerAddr;
 
+	/* SETTING GRID */
+
+	memset(_grid, 0, squareNumber * squareNumber);
+
 	/* SETTING GRID RECT */
 
 	_gridBackgroundRect.width = 800;
 	_gridBackgroundRect.height = 800;
 	_gridBackgroundRect.top = 0;
 	_gridBackgroundRect.left = (WINDOW_WIDTH - _gridBackgroundRect.width) / 2;
+
+	_squareSize.x = _gridBackgroundRect.width / 19;
+	_squareSize.y = _gridBackgroundRect.height / 19;
 
 	/* CREATING SPRITES AND TEXTURES */
 
@@ -38,7 +45,17 @@ PVPModel::~PVPModel()
 bool PVPModel::Clicked(float x, float y)
 {
 	if (_gridBackgroundRect.contains(x, y))
-		std::cout << "Click on grid" << std::endl;
+	{
+		int X;
+		int Y;
+
+		X = floor((x - _gridBackgroundRect.left) / _squareSize.x);
+		Y = floor((y - _gridBackgroundRect.top) / _squareSize.y);
+		std::cout << X << " : " << Y << std::endl;
+		std::cout << "1" << std::endl;
+		_grid[X][Y] = 1;
+		std::cout << "2" << std::endl;
+	}
 	return true;
 }
 
@@ -46,5 +63,16 @@ void PVPModel::Display(sf::RenderWindow *window)
 {
 	window->clear();
 	window->draw(_gridBackground);
+	for (unsigned int y = 0; y < squareNumber; y++)
+	{
+		for (unsigned int x = 0; x < squareNumber; x++)
+		{
+			if (_grid[x][y] == 1)
+			{
+				_pawnsSprites[0].setPosition(240/*_gridBackgroundRect.left + _squareSize.x * x*/, 0/*_gridBackgroundRect.top + _squareSize.y * y*/);
+				window->draw(_pawnsSprites[0]);
+			}
+		}
+	}
 	window->display();
 }
--- Gomoku/PVPModel.h
+++ Gomoku/PVPModel.h
@@ -7,16 +7,21 @@ class PVPModel :
 {
 private:
 	//const static unsigned int _gridBackgroundSize = 800;
+	const static unsigned int squareNumber = 19;
 
-	sf::IntRect _gridBackgroundRect;
-	std::vector<std::string> _grid;
+	sf::FloatRect _gridBackgroundRect;
+	char _grid[squareNumber][squareNumber];
+	sf::Vector2f _squareSize;
+
 	bool _playerTurn;
 	bool _winningStates[2];
 	char _playersPawnsLeft[2];
+
 	sf::Texture _pawnsTexture[2];
 	sf::Sprite _pawnsSprites[2];
 	sf::Texture _gridBackgroundTexture;
 	sf::Sprite _gridBackground;
+
 	sf::RenderWindow *_window;
 	EventManager **_eventManagerAddr;
 

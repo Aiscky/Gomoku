99e8349d42ba6de84364059f05e7e3b074e3887e
guillaume plantie <guillaume.plantie@epitech.eu>
2016-01-18 14:55:12 +0100

    grid.cpp

---
+++ Gomoku/Grid.cpp
@@ -0,0 +1,21 @@
+#include "Grid.h"
+
+Grid::Grid()
+{
+	memset(_grid, 0, squareNumber * squareNumber);
+}
+
+unsigned int Grid::getSideSize()
+{
+	return (squareNumber);
+}
+
+void Grid::addPaw(int x, int y, int color)
+{
+	_grid[x][y] = color;
+}
+
+int Grid::getCell(int x, int y)
+{
+	return (_grid[x][y]);
+}

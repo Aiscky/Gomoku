fd15a6219824f5abe3b91c74980a4cd557a806c7
guillaume plantie <guillaume.plantie@epitech.eu>
2016-01-19 13:20:33 +0100

    AI

--- Gomoku/AI.cpp
+++ Gomoku/AI.cpp
@@ -1,15 +1,25 @@
 #include "AI.h"
 
-AI::AI()
-{
-}
-
 AI::AI(Grid *grid)
 {
 	_grid = grid;
+	srand(time(NULL));
 }
 
 void AI::play()
 {
-	std::cout << "Bot plays" << std::endl;
+	eval();
+	minmax();
+	static int x = 10;
+	int y = 9;
+	_grid->addPawn(x, y, Grid::BLACK);
+	x++;
+}
+
+void eval()
+{
+}
+
+void minmax()
+{
 }
--- Gomoku/Grid.h
+++ Gomoku/Grid.h
@@ -18,7 +18,7 @@ public:
 	void addPawn(char x, char y, PlayerColor color);
 	char getCell(char x, char y);
 	unsigned int getSideSize();
-	void affGrid();
+	//void affGrid();
 
 private:
 

cca6ce890dd312a8e8793984971be006efedf992
guillaume plantie <guillaume.plantie@epitech.eu>
2016-01-20 18:13:45 +0100

    Nothing to say

--- Gomoku/AI.cpp
+++ Gomoku/AI.cpp
@@ -8,12 +8,10 @@ AI::AI(Grid *grid)
 
 void AI::play()
 {
-	int depth = 3;
-	char bestX = _grid->getLeft();
-	char bestY = _grid->getTop();
-	int tmp;
-	_alpha = -1000;
-	_beta = 1000;
+	int depth = 2;
+	char bestX = 0;
+	char bestY = 0;
+	int max = -1000;
 
 	for (char y = _grid->getTop(); y <= _grid->getBottom(); y++)
 	{
@@ -22,10 +20,11 @@ void AI::play()
 			if (_grid->getCell(x, y) == 0)
 			{
 				_grid->addPawn(x, y, Grid::BLACK);
-				tmp = Min(depth - 1);
-				if (tmp > _alpha)
+				int tmp = Min(depth - 1);
+				// std::cout << "Max = " << (int)y << ":" << (int)x << " -  " << tmp << std::endl;
+				if (tmp > max)
 				{
-					_alpha = tmp;
+					max = tmp;
 					bestX = x;
 					bestY = y;
 				}
@@ -33,12 +32,13 @@ void AI::play()
 			}
 		}
 	}
+	std::cout << "Value = " << _value << std::endl;
 	_grid->addPawn(bestX, bestY, Grid::BLACK);
 }
 
 int AI::Min(int depth)
 {
-	int tmp;
+	int min = 1000;
 	if (depth == 0)
 		return (Eval());
 	for (char y = _grid->getTop(); y <= _grid->getBottom(); y++)
@@ -47,22 +47,20 @@ int AI::Min(int depth)
 		{
 			if (_grid->getCell(x, y) == 0)
 			{
-				_grid->addPawn(x, y, Grid::BLACK);
-				tmp = Max(depth - 1);
+				_grid->addPawn(x, y, Grid::WHITE);
+				int tmp = Max(depth - 1);
+				if (tmp < min)
+					min = tmp;
 				_grid->deletePawn(x, y);
-				if (tmp < _beta)
-					_beta = tmp;
-				if (_beta <= _alpha)
-					return (_beta);
 			}
 		}
 	}
-	return (_beta);
+	return (min);
 }
 
 int AI::Max(int depth)
 {
-	int tmp;
+	int max = -1000;
 	if (depth == 0)
 		return (Eval());
 	for (char y = _grid->getTop(); y <= _grid->getBottom(); y++)
@@ -72,20 +70,89 @@ int AI::Max(int depth)
 			if (_grid->getCell(x, y) == 0)
 			{
 				_grid->addPawn(x, y, Grid::BLACK);
-				tmp = Min(depth - 1);
+				int tmp = Min(depth - 1);
+				if (tmp > max)
+					max = tmp;
 				_grid->deletePawn(x, y);
-				if (tmp > _alpha)
-					_alpha = tmp;
-				if (_beta < _alpha)
-					return (_alpha);
 			}
 		}
 	}
-	return (_alpha);
+	return (max);
 }
+
+/* EVALUATION AI */
 
 int AI::Eval()
 {
-	return (1);
+	_value = 0;
+	getSeries();
+	return (_value);
 }
 
+void AI::getSeries()
+{
+	//En ligne;
+	for (char y = _grid->getTop(); y <= _grid->getBottom(); y++)
+	{
+		int white = 0;
+		for (char x = _grid->getLeft(); x <= _grid->getRight(); x++)
+		{
+			if (_grid->getCell(x, y) == Grid::WHITE)
+			{
+				white++;
+				if (white == 3)
+					_value -= 50;
+			}
+			else
+				white = 0;
+		}
+	}
+}
+
+/*
+			else if (_grid->getCell(x, y) == Grid::BLACK)
+			{
+				black++;
+				white = 0;
+				if (black == 3)
+					_value += 50;
+			}
+			else
+			{
+				black = 0;
+				white = 0;
+			}
+		}	
+	}
+	//En Colonne;
+	for (char x = _grid->getTop(); x <= _grid->getBottom(); x++)
+	{
+		black = 0;
+		white = 0;
+		for (char y = _grid->getLeft(); y <= _grid->getRight(); y++)
+		{
+			if (_grid->getCell(x, y) == Grid::WHITE)
+			{
+				white++;
+				black = 0;
+				if (white == 3)
+					_value += 50;
+				if (white == 4)
+					_value += 100;
+			}
+			else if (_grid->getCell(x, y) == Grid::BLACK)
+			{
+				black++;
+				white = 0;
+				if (black == 3)
+					_value += 50;
+			}
+			else
+			{
+				black = 0;
+				white = 0;
+			}
+		}
+	}
+}
+*/
